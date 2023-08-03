eac8e1f7d4fa4c02bf7071a29d2cefbecce9d9c5
Luke Nelson <ldnelson16@gmail.com>
2023-08-02 21:53:50 -0400

    Making basic homepage

---
+++ components/homepage.js
@@ -0,0 +1,19 @@
+//imports
+import styles from '../styles/homepage.module.css';
+import textstyles from '../styles/texts.module.css';
+import Link from 'next/link';
+
+//export default 
+export default function HomePage(){
+  return(
+    <>
+      <div className={styles.body}>
+        <div className={styles.centerbox}>
+          <div className={styles.loginheading}>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
