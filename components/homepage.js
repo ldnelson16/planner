ed4e628892e2cf51c93f7c32b5d640169b3bf0b2
Luke Nelson <ldnelson16@gmail.com>
2023-08-04 23:00:06 -0400

    almost done with homepage

--- components/homepage.js
+++ components/homepage.js
@@ -10,13 +10,20 @@ export default function HomePage(){
       <div className={styles.body}>
         <div className={styles.centerbox}>
           <div className={styles.loginside}>
-            <div className={styles.loginheading}>
-              <span className={textstyles.heading}>Log In</span>
+            <div className={styles.login}>
+              <div className={styles.loginheading}>
+                <text className={textstyles.heading}>Log In</text>
+              </div>
+              <input type="text" className={styles.input} placeholder='username'></input>
+              <input type="password" className={styles.input} placeholder='password'></input>
+              <button className={styles.signinbutton}>Log In</button>
             </div>
           </div>
           <div className={styles.signinside}>
-            <div className={styles.loginheading}>
-              <span className={textstyles.heading}>Sign Up</span>
+            <div className={styles.login}>
+              <div className={styles.loginheading}>
+                <span className={textstyles.heading}>Sign Up</span>
+              </div>
             </div>
           </div>
         </div>
