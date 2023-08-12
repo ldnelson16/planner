19022d54121204d2bcdc5b43b918a09bd3d4c5d4
Luke Nelson <ldnelson16@gmail.com>
2023-08-12 12:29:05 -0400

    attempt to integrate u/p

--- components/homepage.js
+++ components/homepage.js
@@ -3,13 +3,39 @@ import styles from '../styles/homepage.module.css';
 import textstyles from '../styles/texts.module.css';
 import Link from 'next/link';
 import {useState} from 'react';
+import updata from '../data/upkeys.json';
+//import fs from 'fs';
 
 //export default 
 export default function HomePage(){
   //Event listeners
+  const signup = () => {
+    console.log("Attempting to signup");
+    console.log(username.toLowerCase()+" "+password);
+    //Checking for username
+    if(updata["usernames"].includes(username.toLowerCase())){
+      console.log("Username "+username+" already exists.");
+      fs.writeFileSync('../data/upkeys.json',JSON.stringify(updata));
+      //display error message for user to sign in
+
+    }
+    else{
+      updata["usernames"]+=[username.toLowerCase()];
+    }
+  }
   const login = () => {
     console.log("Looking for username/password pair");
     //Check database for u/p pair
+    console.log(username.toLowerCase()+"/"+password);
+    if(updata["key_pairs"].includes(username.toLowerCase()+"/"+password)){
+      console.log("Key pair found");
+      //Enter next page
+
+    }
+    else{
+      console.log("Key pair not found, try again.");
+      //prompt error message
+    }
     console.log("Button clicked");
   }
   const enterusername = (event) => {
@@ -21,6 +47,16 @@ export default function HomePage(){
     setPassword(event.target.value);
     console.log(password);
   }
+  const enterfname = (event) => {
+    console.log("First name changed");
+    setFname(event.target.value);
+    console.log(fname);
+  }
+  const enterlname = (event) => {
+    console.log("Last name changed");
+    SetLname(event.target.value);
+    console.log(lname);
+  }
   const handleSUHover = () => {
     setSUHoverState(true);
     console.log("Mouse Entered Sign Up Box");
@@ -31,8 +67,10 @@ export default function HomePage(){
   }
   //useStates
   const [signuphover,setSUHoverState] = useState(false);
-  const [username,setUsername] = useState(0);
-  const [password,setPassword] = useState(0);
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [fname,setFname] = useState("");
+  const [lname,SetLname] = useState("");
   //variable CSS states
   const logindiv = {
     display: signuphover?"flex":"none"
@@ -62,12 +100,12 @@ export default function HomePage(){
                 <span className={textstyles.heading}>Sign Up</span>
               </div>
               <div className={styles.namediv}>
-                <input type="text" className={styles.input3} placeholder="first name"></input>
-                <input type="text" className={styles.input3} placeholder="last name"></input>
+                <input onChange={enterfname} name="fname" type="text" className={styles.input3} placeholder="first name"></input>
+                <input onChange={enterlname} name="lname" type="text" className={styles.input3} placeholder="last name"></input>
               </div>
-              <input type="text" className={styles.input2} placeholder='username'></input>
-              <input type="password" className={styles.input2} placeholder='password'></input>
-              <button className={styles.yellowsigninbutton}>Sign Up</button>
+              <input onChange={enterusername} name="username" type="text" className={styles.input2} placeholder='username'></input>
+              <input onChange={enterpassword} name="password" type="password" className={styles.input2} placeholder='password'></input>
+              <button type="submit" onClick={signup} className={styles.yellowsigninbutton}>Sign Up</button>
             </div>
           </div>
           <div onMouseEnter={handleSUHover} style={signindiv} className={styles.signinsidecover}><span className={textstyles.h1}>Sign Up</span></div>
